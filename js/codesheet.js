e1b6993d8ac03436162587d7eaab7f0d2ec3109b
Shatrughn Gupta <shatrughn.gupta@gmail.com>
2018-10-06 01:13:34 -0700

    chore: mention a different example in FAQ

--- js/codesheet.js
+++ js/codesheet.js
@@ -21,7 +21,6 @@ const sampleCodes = {
     '}'
   ].join('\n'),
   java: [
-    'import java.util.Arrays;',
     'class HelloWorld {',
     '  public static void main(String[] args) {',
     '    System.out.println("Hello World from Java!");',
